5576160918d5fd6e923acb96d741be8dc49307f4
Paul Anderson <Paul.Robert.Anderson@gmail.com>
2013-04-15 11:42:04 +0530

    Added Monitor and Uploader, Licence Info

--- WeatherMonitorAndUploader.rb
+++ WeatherMonitorAndUploader.rb
@@ -1,3 +1,21 @@
+# Paul's Easy Weather file monitor and uploader. Monitors easyweather.dat for changes, and renders a HTML page and JSON object, uploading them to an FTP server.
+# To use: Set the paths and FTP variables as needed. Run Easy Weather, then run this program. 
+# Copyright (C) 2013 Paul Anderson
+# 
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
 require 'net/ftp'
 require 'json'
 require 'date'
